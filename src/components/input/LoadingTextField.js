58ebb2247c048c4736d6f2039d485949fffdfc39
Liam Elliott <liamelliott1123@gmail.com>
2019-02-19 15:56:28 -0700

    Add Defaults and Types to Props

--- src/components/input/LoadingTextField.js
+++ src/components/input/LoadingTextField.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { Paper, InputBase, CircularProgress, Divider, IconButton } from '@material-ui/core';
 
 class LoadingTextField extends React.Component {
+
+    handleChange = (event) => {
+        if (event.target.value.length >= this.props.minLength) {
+
+        }
+    }
+
     render() {
         return (
             <Paper>
-                <InputBase placeholder={this.props.placeholder} />
+                <InputBase type="text" placeholder={this.props.placeholder} onChange={this.handleChange} />
                 <CircularProgress variant="determinate" />
             </Paper>
         );
     }
 }
+
+LoadingTextField.propTypes = {
+    placeholder: PropTypes.string,
+    minLength: PropTypes.number
+};
+
+LoadingTextField.defaultProps = {
+    placeholder: '',
+    minLength: 3
+};
+
+export default LoadingTextField;
