eab1fc8db05f9f798ce456d656d11ed368b31fdb
Liam Elliott <liamelliott1123@gmail.com>
2019-02-20 15:56:13 -0700

    Add SearchableInput to App

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,7 @@ class App extends React.Component {
         return (
             <div>
                 <h1>Adventure Admin</h1>
+                <SearchableInput />
             </div>
         )
     }
