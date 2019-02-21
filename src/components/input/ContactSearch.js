99382ac207e4340710ba2603f4b973e8b159e08e
Liam Elliott <liamelliott1123@gmail.com>
2019-02-21 10:50:39 -0700

    Add Contacts State to ContactSearch

--- src/components/input/ContactSearch.js
+++ src/components/input/ContactSearch.js
@@ -3,16 +3,34 @@ import PropTypes from 'prop-types';
 import axios from 'axios';
 
 import SearchBox from './SearchBox';
+import ContactsDisplay from '../display/ContactsDisplay';
 
 class ContactSearch extends React.Component {
-    handleSearch = (input) => new Promise((resolve, reject) => {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            contacts: []
+        }
+    }
+
+    handleSearch = (input) => new Promise((resolve, reject) => {
+        this.setState(Object.assign(this.state, {
+            contacts: [
+                { name: 'Liam Elliott', avatar: 'http://i.pravatar.cc/200', email: 'lelliott@alpineclubofcanada.ca' },
+                { name: 'Jeff Lockyer', avatar: 'https://alpineclubofcanada.ca/WEB/images/ACC/About/National%20Office/Jeff%20in%20fur_thumb.jpg', email: 'jlockyer@alpineclubofcanada.ca' },
+                { name: 'Chris Petrauskas', avatar: 'https://alpineclubofcanada.ca/WEB/images/ACC/About/National%20Office/ChrisPetrauskas_thumb.jpg', email: 'cpetrauskas@alpineclubofcanada.ca' },
+                { name: 'Lawrence White', avatar: 'https://alpineclubofcanada.ca/WEB/images/ACC/About/National%20Office/LW%20gunslinger%20staff.jpg', email: 'lwhite@alpineclubofcanada.ca' }
+            ]
+        }));
     });
 
     render() {
         return (
             <React.Fragment>
                 <SearchBox onSearch={this.handleSearch} />
+                <ContactsDisplay contacts={this.state.contacts} />
             </React.Fragment>
         );
     }
