34c3d4de564d6994110a2f33665d5523128378b8
Liam Elliott <liamelliott1123@gmail.com>
2019-02-25 16:38:34 -0700

    Add AdminDisplay Component

---
+++ src/components/display/AdminDisplay.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Paper, Typography, withStyles } from '@material-ui/core';
+
+import ContactSelect from './ContactSelect';
+
+const styles = {
+
+};
+
+const AdminDisplay = (props) => {
+    const { classes } = props;
+
+    return (
+        <Paper>
+            {administrators.length > 0 ? <ContactSelect contacts={this.props.administrators} /> : <Typography>There are no administrators assigned to this adventure.</Typography>}
+        </Paper>
+    );
+}
+
+AdminDisplay.propTypes = {
+    administrators: PropTypes.array
+};
+
+AdminDisplay.defaultProps = {
+    administrators: []
+};
+
+export default withStyles(styles)(AdminDisplay);
