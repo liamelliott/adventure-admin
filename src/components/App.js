54cbc9d1034e7328ec20b83e40210053127082f6
Liam Elliott <liamelliott1123@gmail.com>
2019-03-05 16:27:37 -0700

    Initialize State in Constructor

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,20 @@ import { Typography } from '@material-ui/core';
 import axios from 'axios';
 
 import ManageAdministratorsPage from './input/ManageAdministratorsPage';
-import { apiToken } from '../../secrets';
+import { apiToken } from '../../config/secrets';
 
 class App extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {};
     }
 
-    componentDidMount = () => {
-        const token = apiToken || document.getElementById('__RequestVerificationToken').value;
+    componentDidMount() {
+        const token = apiToken || { type: 'csrf', value: document.getElementById('__RequestVerificationToken').value };
         this.setState(Object.assign(this.state, { apiToken: token }));
-    };
+    }
 
     handleSearch = (query) => new Promise((resolve, reject) => {
         resolve([]);
