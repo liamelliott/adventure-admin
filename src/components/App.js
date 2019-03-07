276a327dca9898254cf2c14943e9667c8defcc41
Liam Elliott <liamelliott1123@gmail.com>
2019-03-07 16:44:20 -0700

    Add Reject to Promises

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import { Typography } from '@material-ui/core';
 import axios from 'axios';
 
 import ManageAdministratorsPage from './input/ManageAdministratorsPage';
-import { apiToken } from '../../config';
+import { apiToken, baseURL } from '../../config';
 import { CsrfToken, BearerToken } from '../connections/Token';
 
 class App extends React.Component {
@@ -11,30 +11,34 @@ class App extends React.Component {
     constructor(props) {
         super(props);
 
-        axios.defaults.baseURL = ;
-    }
-
-    componentDidMount() {
+        axios.defaults.baseURL = baseURL;
         const token = apiToken ? new BearerToken(apiToken) : new CsrfToken(document.getElementById('__RequestVerificationToken').value);
         token.connect(axios);
     }
 
     handleSearch = (query) => new Promise((resolve, reject) => {
-        axios.get().then((response) => {
+        console.log(axios.defaults);
+        axios.get(`/party?name=contains:${query}`).then((response) => {
             resolve([]);
-        }).catch();
+        }).catch((error) => {
+            reject();
+        });
     });
 
     handleAdd = (contact) => new Promise((resolve, reject) => {
         axios.post().then((response) => {
             resolve();
-        }).catch();
+        }).catch((error) => {
+            reject();
+        });
     });
 
     handleRemove = (contact) => new Promise((resolve, reject) => {
         axios.delete().then((response) => {
             resolve();
-        }).catch();
+        }).catch((error) => {
+            reject();
+        });
     });
 
     render() {
