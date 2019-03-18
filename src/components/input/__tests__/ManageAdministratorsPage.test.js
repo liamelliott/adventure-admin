d0c71f67cd377495426abdf216d9af609510126d
Liam Elliott <liamelliott1123@gmail.com>
2019-03-18 17:25:36 -0600

    Change Placeholder Text for Name and Email

--- src/components/input/__tests__/ManageAdministratorsPage.test.js
+++ src/components/input/__tests__/ManageAdministratorsPage.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import ManageAdministratorsPage from '../ManageAdministratorsPage';
-import { isTSAnyKeyword, exportAllDeclaration } from '@babel/types';
 
 describe('<ManageAdministratorsPage />', () => {
     it.todo('should add an item to administrators on add');
